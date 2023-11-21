d7edb57590499dd2a1faafc9a3a1dd689c254529
Diego Fornalha <diegofornalha@gmail.com>
2023-11-21 15:47:43 -0300

    fist commitg

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+// vite.config.js
+import { sveltekit } from '@sveltejs/kit/vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import inject from '@rollup/plugin-inject'
+
+/** @type {import('vite').UserConfig} */
+const config = {
+    define: {
+        global: 'globalThis'
+    },
+    plugins: [
+        nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+        sveltekit()
+    ],
+    resolve:{
+        alias:{
+            "node-fetch": "./node_modules/node-fetch/browser.js"
+        }
+    },
+    build: {
+        rollupOptions: {
+            plugins: [inject({ Buffer: ['buffer', 'Buffer'] })]
+        },
+    },
+};
+export default config;
